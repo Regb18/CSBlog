323fef5e4b40d009ee3c4bd231ae3bbbb7e68139
Regb18 <reginald.ac.barnes@gmail.com>
2023-02-14 12:56:36 -0500

    template, layout, home index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using CSBlog.Models;
+﻿using CSBlog.Data;
+using CSBlog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CSBlog.Controllers
@@ -7,15 +9,23 @@ namespace CSBlog.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IEnumerable<BlogPost> blogPosts = await _context.BlogPosts
+                                                            .Include(b => b.Category)
+                                                            .Include(b => b.Tags)
+                                                            .Where(b => b.IsPublished == true && b.IsDeleted == false)
+                                                            .ToListAsync();
+
+            return View(blogPosts);
         }
 
         public IActionResult Privacy()
